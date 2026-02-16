75c640cc17678269410610ff3d0f3ef39c69df4b
hkcseihjk-byte <hkcseihjk@gmail.com>
2026-02-16 12:24:23 +0600

    Footer.tsx

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+  return (
+    <div className="text-center p-5 bg-dark text-[#ecf0f1] text-[13px] mt-[30px]">
+      © 2025 Dally Update 02. All Rights Reserved.
+    </div>
+  );
+};
+
+export default Footer;
